5235e02a993aebc496c924c97b7101a5089e35f2
Wiranzoi <ngowitan@yahoo.fr>
2019-03-31 22:29:35 +0200

    First Commit

---
+++ public/elements/header.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= $title??'GameNchange.fr'?></title>
+    <link rel="stylesheet" href="bootstrap-4.3.1-dist\css\bootstrap.min.css">
+    
+</head>
+<body>
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+
+  <a class="navbar-brand" href=""><img class="img-fluid" width="36" height="36" src="assets\logo.svg" alt="GameNchange.fr"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarsExample03">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="">Accueil <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+        <div class="dropdown-menu" aria-labelledby="dropdown03">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-md-0">
+      <input class="form-control" type="text" placeholder="Search">
+    </form>
+  </div>
+     <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Se Connecter
+</button>
+
+</nav>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Se Connecter</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form  action="" method="post">
+            <div class="form-group">
+                <label for="login">Votre identifiant</label>
+                <input type="text" name="login" id="" class="form-control" placeholder="Votre identifiant">
+                <div class="invalid-feedback"><?= $errors['login'] ?? '' ?></div>
+
+                <label for="password">Votre mot de passe</label>
+                <input type="text" name="login" id="" class="form-control" placeholder="Votre mot de passe">
+                <div class="invalid-feedback"><?= $errors['password'] ?? '' ?></div>
+
+            </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Valider</button>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ public/index.php
@@ -0,0 +1,52 @@
+<?php
+$title = 'Accueil';
+?>
+<?php require 'elements' . DIRECTORY_SEPARATOR . 'header.php'; ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-xl-6">
+            <div class="alert alert-primary" role="alert">
+                A simple primary alert—check it out!
+            </div>
+        </div>
+        <div class="col-sm border">
+            <h1>COUCOU</h1>
+        </div>
+        <div class="col-sm">
+            <h1 class="text-danger">COUCOU</h1>
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="col-xl-6">
+            <div class="alert alert-primary" role="alert">
+                A simple primary alert—check it out!
+            </div>
+        </div>
+        <div class="col-sm border">
+            <h1>COUCOU</h1>
+        </div>
+        <div class="col-sm">
+            <h1 class="text-danger">COUCOU</h1>
+        </div>
+
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-xl-6">
+            <div class="alert alert-primary" role="alert">
+                A simple primary alert—check it out!
+            </div>
+        </div>
+        <div class="col-sm border">
+            <h1>COUCOU</h1>
+        </div>
+        <div class="col-sm">
+            <h1 class="text-danger">COUCOU</h1>
+        </div>
+
+    </div>
+</div>
+<?php require 'elements' . DIRECTORY_SEPARATOR . 'footer.php';
